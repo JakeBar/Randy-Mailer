918a0594b8299fbffff5707e28f00d43daefe301
JakeBar <Jake.B.Dev@gmail.com>
2017-02-09 18:23:59 +1100

    Refactoring, Fixed bugs with README.md, duplicate wave graphs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,49 +3,54 @@ class UserMailer < ApplicationMailer
 
     def surf_report(user, email)
 
-      # get surf data
-      url = 'http://magicseaweed.com/api/1/forecast/?spot_id=1070'
-      uri = URI(url)
-      response = Net::HTTP.get(uri)
-      result = ActiveSupport::JSON.decode(response)
+      result = get_surf_data()
 
       # store applicable data
       data = User.new
       data.login =  user
       data.email =  email
-
-      # get reports for the next 24 hours
-      time = Time.now.to_i
-
-      daily_data = Array.new
-      result.each do |item|
-
-        # if the report time has already passed,
-        # OR
-        # it's more than 24 hours later than local time
-        # remove from the stack
-        if (item['localTimestamp'] >= (time + 60 * 60 * 9) ) && (item['localTimestamp'] <= (time + 60 * 60 * 24))
-          daily_data.push(item)
-        end
-      end
-
-      data.surf_data = daily_data
-
-      # get the average swell score
-      swell_values = Array.new
-      data.surf_data.each do |item|
-        swell_values.push(item['solidRating'])
-      end
-
-      swell_score = (swell_values.sum / swell_values.size.to_f).round
-
-      data.swell_score = swell_score
+      data.surf_data = filter_surf_results(result)
+      data.swell_score = get_surf_score(data.surf_data)
       @user = data
       @url  = 'http://randy.com/login'
 
-      # given that the swell score is greater than 3, send email
-      # if (data.swell_score >= 3)
+      # given that the swell score is greater than x, send email
+      # if (data.swell_score > 2)
         mail(to: @user.email, subject: 'Surf Report - Fairhaven')
       # end
     end
+
+  # get_surf_data() - get the surf data from MagicSeaweed API
+  def get_surf_data()
+    url = 'http://magicseaweed.com/api/1/forecast/?spot_id=1070'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    return ActiveSupport::JSON.decode(response)
+  end
+
+  # filter_surf_results(surf_data) - filter out results outside the next 24 hours
+  def filter_surf_results(surf_data)
+    current_time = Time.now.to_i
+    data = Array.new
+
+    surf_data.each do |item|
+      # if the report time has already passed,
+      # OR
+      # it's more than 24 hours later than local time
+      # remove from the stack
+      if (item['localTimestamp'] >= (current_time + 60 * 60 * 9) ) && (item['localTimestamp'] <= (current_time + 60 * 60 * 24))
+        data.push(item)
+      end
+    end
+    return data
+  end
+
+  # evaluate_surf_score(surf_data) - return the average swell score
+  def get_surf_score(surf_data)
+    swell_values = Array.new
+    surf_data.each do |item|
+      swell_values.push(item['solidRating'])
+    end
+    return (swell_values.sum / swell_values.size.to_f).round
+  end
 end
--- lib/tasks/email_tasks.rake
+++ lib/tasks/email_tasks.rake
@@ -3,7 +3,7 @@
 desc 'send surf report'
 task send_surf_report: :environment do
   puts "sending surf report(s)"
-  emails = [["Jake@School", "s3380519@student.rmit.edu.au"],["Jake@Work","jake.b.dev@gmail.com"]]
+  emails = [["Jake@School", "s3380519@student.rmit.edu.au"],["Jake@Work","jake.b.dev@gmail.com"],["Ollie","obeckerscott@gmail.com"]]
 
   emails.each do |item|
     UserMailer.surf_report(item[0], item[1]).deliver!
